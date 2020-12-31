8479727e555b981e41d2f4e370602b8dd96c81e9
lacomaco <lacomaco122@gmail.com>
2020-12-31 22:00:39 +0900

    프로그래머스 : 수식_최대화

---
+++ 영근/수식_최대화.js
@@ -0,0 +1,72 @@
+const priorities = [];
+const expression = ['*','-','+'];
+const mark = [false,false,false];
+function solution(expression){
+  let answer = 0;
+  shuffle(0,[]);
+  priorities.forEach(priority=>{
+    answer = Math.max(answer,Math.abs(run(priority,expression,0)));
+  });
+  return answer;
+}
+
+function shuffle(position,array){
+  if(position===3){
+    priorities.push([...array]);
+    return;
+  }
+
+  for(let i=0;i<expression.length;i++){
+    if(!mark[i]){
+      array.push(expression[i]);
+      mark[i]=true;
+      shuffle(position+1,array);
+      array.pop();
+      mark[i]=false;
+    }
+  }
+}
+
+function run(priority,expression){
+  return splitExpression(priority,expression,0);
+}
+
+function splitExpression(priority,express,n){
+  if(n==3){
+    return Number(express);
+  }
+  const values = [];
+  express.split(priority[n]).forEach((curr)=>{
+    let test=curr;
+    if(!isNumber(curr)){
+      test = splitExpression(priority,curr,n+1);
+    }
+    values.push(test);
+  });
+  return calcExpress(priority[n],values);
+}
+
+function calcExpress(express,numbers){
+  if(express==='*'){
+    return numbers.reduce((acc,curr)=>{
+      return acc*Number(curr);
+    },1);
+  }else if(express==='+'){
+    return numbers.reduce((acc,curr)=>{
+      return acc + Number(curr);
+    },0);
+  }else{
+    return numbers.slice(1).reduce((acc,curr,id)=>{
+      return acc-Number(curr);
+    },numbers[0]);
+  }
+}
+
+function isNumber(number){
+  return !Number.isNaN(Number(number));
+}
+
+const temp = "100-200*300-500+20";
+console.log(solution(temp));
+const temp2 = "50*6-3*2";
+console.log(solution(temp2));
